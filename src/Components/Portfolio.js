1342ad62e63c4379a04ba954120fca8f16379ed7
Premkumar <chandaprem999@gmail.com>
2021-12-06 02:41:19 +0530

    adding project images

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -21,7 +21,7 @@ class Portfolio extends Component {
                     rel="noopener noreferrer"
                   >
                     <img
-                      src="images/thumbs/shoppers-stop-clone.png"
+                      src="https://github.com/chandaprem999/Portfolio/tree/master/build/images/thumbs/shoppers-stop-clone.png"
                       alt="js-capstone"
                     />
                   </a>
@@ -79,7 +79,7 @@ class Portfolio extends Component {
                     rel="noopener noreferrer"
                   >
                     <img
-                      src="images/thumbs/TVC-mall-clone.png"
+                      src="https://github.com/chandaprem999/Portfolio/tree/master/build/images/thumbs/TVC-mall-clone.png"
                       alt="react-capstone"
                     />
                   </a>
